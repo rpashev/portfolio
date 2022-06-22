ec6d3d61a58fdb3515e2eae452633a18cf96854e
rpashev <rossen1991@gmail.com>
2022-06-22 17:30:49 +0300

    new favicon, meta data

--- pages/_app.js
+++ pages/_app.js
@@ -8,12 +8,17 @@ function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
       <Head>
+        <meta
+          name="description"
+          content="A portfolio application for Rosen Pashev, an aspiring web developer from Bulgaria specializing in JavaScript, CSS, SCSS, HTML, React, Vue, Angular, Node, Mongo, Express, TypeScript"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <title>rpashev</title>
       </Head>
       <Header />
       <Component {...pageProps} />
